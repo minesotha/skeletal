22d1f308930bb9a1ffabe839ea0cf7b97e5ff40c
Maja Trzepacz <minesotha@gmail.com>
2020-04-02 22:03:43 +0200

    few real bones, implemented movement and starting position

--- bones/BoneClass.js
+++ bones/BoneClass.js
@@ -1,5 +1,21 @@
-var Bone = class Bone{
-  constructor(path) {
+export default class Bone {
+  constructor(path, name, startingX = 0, startingY = 0) {
     this.path = path;
+    this.name = name;
+    this.svg = null;
+    this.x = startingX;
+    this.y = startingY;
+  }
+
+  setSvg(svg) {
+    this.svg = svg;
+    $("#" + this.name).css("top", this.y + 'px');
+    $("#" + this.name).css("left", this.x + 'px');
+  }
+
+  calculateBoundingBox() {
+    var bb = this.svg.bbox();
+    $("#" + this.name).css("width", bb.width + 'px');
+    $("#" + this.name).css("height", bb.height + 'px');
   }
 }
--- index.js
+++ index.js
@@ -1,44 +1,60 @@
-function loadBone() {
+import Skeleton from "./bones/Skeleton.js";
+
+var skeleton = new Skeleton();
+var boneColor = '#e3dac9';
+
+function loadBone(bone) {
     var draw = SVG().addTo('#birdImg');
-    var path = draw.path(bone1.path);
-    path.fill('#f06').move(0, 0)
-    path.stroke({ color: '#f06', width: 4, linecap: 'round', linejoin: 'round' });
+    draw.attr("id",bone.name);
+    var path = draw.path(bone.path);
+    path.fill(boneColor).move(0, 0);
+    path.stroke({ color: 'black', width: 1, linecap: 'round', linejoin: 'round' });
+    bone.setSvg(draw);
+    bone.calculateBoundingBox();
     path.on(['mouseover'], highlightOn);
     path.on(['mouseout'], highlightOff);
 }
 
 var highlightOn = function () {
-    this.fill({ color: '#fff' })
-    var element = this;
-    $('#birdImg').on("mousedown", null, element, (event, element) => moveElement(event));
+    this.parents()[0].on("mousedown", onSvgMouseDown);
+    this.fill({ color: '#dfd0b7' })
 }
-
-function moveElement(event) {
-    var element = event.data;
-    $('#birdImg').on("mousemove", null, element,  (event, element) => {
-        var element = event.data;
-        element.center(event.pageX, event.pageY);
-    })
-}
-
 
 var highlightOff = function () {
-    this.fill({ color: '#000' })
-
+    this.parents()[0].off("mousedown", onSvgMouseDown);
+    this.fill({ color: boneColor})
 }
 
-var mouseDown = function () {
-    this.move(10, 10);
+var offset = [0, 0];
+
+var onSvgMouseDown = function (e) {
+    var svg = this;
+    $(document).on("mousemove", null, svg, (event, svg) => moveSvg(event));
+    $(svg).off("mousedown", onSvgMouseDown);
+    var id = this.id();
+    var elementOffset = $("#"+id).offset();
+    offset = [
+        elementOffset.left - e.clientX,
+        elementOffset.top - e.clientY
+    ];
 }
 
-// or if you want to pass an event
-function whenSomethingHappens(event) {
-    element.fire(event)
+var moveSvg = function (e) {
+    e.preventDefault();
+    let mousePosition = {
+        x: event.clientX,
+        y: event.clientY
+    };
+    var id = e.data.id();
+    $("#"+id).css("top", (mousePosition.y + offset[1]) + 'px');
+    $("#"+id).css("left", (mousePosition.x + offset[0]) + 'px');
 }
 
 $(function () {
-    $('#birdImg').mouseup(() => { $('#birdImg').off("mousedown"); $('#birdImg').off("mousemove"); });
-
-    loadBone();
+    $(document).on("mouseup", () => { $(document).off("mousemove") });
+    var bones = skeleton.getBones();
+    for (let i = 0; i < bones.length; i++) {
+        loadBone(bones[i]);
+    };
 });
 
